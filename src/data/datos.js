9113be95a06bc738c472664439af843fb02a78fd
Pablo Marino <pmarino2013@gmail.com>
2025-10-22 17:33:02 -0300

    Avanzo con página inicio

---
+++ src/data/datos.js
@@ -0,0 +1,19 @@
+const servicios = [
+  {
+    id: 1,
+    servicio: "Refrigeración de Aires acondicionados",
+    icon: "/icono_aire.png",
+  },
+  {
+    id: 1,
+    servicio: "Refrigeración de Aires acondicionados",
+    icon: "/icono_aire.png",
+  },
+  {
+    id: 1,
+    servicio: "Refrigeración de Aires acondicionados",
+    icon: "/icono_aire.png",
+  },
+];
+
+export { servicios };
