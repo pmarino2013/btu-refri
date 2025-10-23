5eaf87981b8e568d21a6cd1deaf2449f254e1b26
Pablo Marino <pmarino2013@gmail.com>
2025-10-23 10:49:31 -0300

    Agrego sección de servicio a domicilio y mejoro los textos de las secciones de index

--- src/data/datos.js
+++ src/data/datos.js
@@ -7,12 +7,12 @@ const servicios = [
   {
     id: 1,
     servicio: "Refrigeración de Aires acondicionados",
-    icon: "/icono_aire.png",
+    icon: "/icono_aire2.png",
   },
   {
     id: 1,
     servicio: "Refrigeración de Aires acondicionados",
-    icon: "/icono_aire.png",
+    icon: "/icono_reparacion.png",
   },
 ];
 
